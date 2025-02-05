43131895c9df122c989945ca5cf316b1389e557b
shingyu <shyu@staredges.cn>
2025-02-06 01:53:23 +0800

    init

---
+++ api.py
@@ -0,0 +1,110 @@
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import FileResponse
+import uvicorn
+# noinspection PyPackageRequirements
+from pydantic import BaseModel
+from redis import asyncio as redis
+import uuid
+from loguru import logger
+from sys import stdout
+
+from utils import config
+
+logger.remove()
+logger.add("logs/api.log", rotation="20 MB", encoding="utf-8", enqueue=True, level=config.logs.level)
+logger.add(stdout, colorize=True, enqueue=True)
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+redis_conn = redis.Redis(host=config.redis.host,
+                         port=config.redis.port,
+                         db=config.redis.db)
+
+
+class TaskRequest(BaseModel):
+    book_id: str
+
+
+@app.post("/tasks")
+async def submit_task(task: TaskRequest, request: Request):
+    task_id = str(uuid.uuid4())
+    platform = request.headers.get("User-Agent", "unknown")
+    logger.info(f"Received task {task_id} from {platform}, book_id: {task.book_id}")
+    if len(task.book_id) != 19:
+        raise HTTPException(status_code=400, detail="Invalid book_id length")
+    # 存储任务元数据
+    await redis_conn.hset(f"task:{task_id}", mapping={
+        "book_id": task.book_id,
+        "status": "pending",
+    })
+    # 任务入队
+    await redis_conn.lpush("task_queue", task_id)
+    return {"task_id": task_id}
+
+
+@app.get("/tasks/{task_id}")
+async def get_task_status(task_id: str):
+    task_data = await redis_conn.hgetall(f"task:{task_id}")
+    if not task_data:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {
+        "book_id": task_data.get(b"book_id").decode(),
+        "status": task_data.get(b"status").decode(),
+    }
+
+
+@app.get("/get/{task_id}")
+async def get_task_status(task_id: str, request: Request):
+    platform = request.headers.get("User-Agent", "unknown")
+    if not platform.startswith("Bot"):
+        raise HTTPException(status_code=403, detail="Forbidden，Only bots are allowed to access this API")
+    task_data = await redis_conn.hgetall(f"task:{task_id}")
+    if not task_data:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {
+        "status": task_data.get(b"status").decode(),
+        "content": task_data.get(b"content", b"").decode(),
+    }
+
+
+@app.get("/download/{task_id}")
+async def download_file(task_id: str, request: Request):
+    task_data = await redis_conn.hgetall(f"task:{task_id}")
+    if not task_data:
+        return templates.TemplateResponse(
+            "status.html",
+            {
+                "request": request,
+                "status": "not_found",
+                "status_code": 404
+            }
+        )
+    status = task_data.get(b"status").decode()
+    context = {
+        "request": request,
+        "status": status,
+    }
+    if status == "pending" or status == "processing":
+        context["status_code"] = 202
+    elif status == "completed":
+        context["status_code"] = 200
+        context["download_url"] = task_data.get(b"url").decode()
+    elif status == "failed":
+        message = task_data.get(b"message", b"").decode()
+        if message:
+            context["status_code"] = 422  # 有信息表示是由于超过章节数量限制而失败
+            context["message"] = message
+        else:
+            context["status_code"] = 500
+            context["message"] = "请检查你提交的书籍ID是否正确和是否被下架，或稍后再试"
+    return templates.TemplateResponse("status.html", context)
+
+
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse("templates/favicon.ico")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=config.main.api.host, port=config.main.api.port)
---
+++ worker.py
@@ -0,0 +1,132 @@
+# worker.py
+from redis import asyncio as redis
+from datetime import timedelta
+import asyncio
+from loguru import logger
+from sys import stdout
+
+from database import get_session, Books, Chapters
+from utils import config, RawChapter, oclient, TooManyChaptersError
+import utils
+
+logger.remove()
+logger.add("logs/worker.log", rotation="20 MB", encoding="utf-8", enqueue=True, level=config.logs.level)
+logger.add(stdout, colorize=True, enqueue=True)
+
+redis_conn = redis.Redis(host=config.redis.host,
+                         port=config.redis.port,
+                         db=config.redis.db)
+db = get_session()
+
+
+async def main():
+    while True:
+        # 异步阻塞获取任务
+        task_id_ = await redis_conn.brpop(["task_queue"])
+
+        task_id = task_id_[1].decode()
+        logger.info(f"Processing task {task_id}")
+
+        try:
+            # 异步标记为处理中
+            await redis_conn.hset(f"task:{task_id}", "status", "processing")
+
+            # 异步执行任务处理
+            await process_task(task_id)
+
+            logger.success(f"Task {task_id} completed")
+
+        except Exception as e:
+            logger.error(f"Task {task_id} failed: {e}")
+            logger.exception(e)
+            await redis_conn.hset(f"task:{task_id}", "status", "failed")
+            if isinstance(e, TooManyChaptersError):
+                await redis_conn.hset(f"task:{task_id}", "message", "你提交的小说章节数量过多，超过3000章限制")
+            await redis_conn.expire(f"task:{task_id}", timedelta(hours=config.ttls.tasks))
+
+
+async def process_task(task_id):
+    # 异步获取任务数据
+    book_id = await redis_conn.hget(f"task:{task_id}", "book_id")
+    book_id = book_id.decode()  # noqa: 返回的是bytes类型，需要解码
+    result = await utils.get_existed_book(book_id, redis_conn, db)
+    if not result:
+        logger.debug(f"Task {task_id}: Book {book_id} not found in cache and database")
+        info = await utils.get_info(book_id, redis_conn)
+        title = info["data"]["book_info"]["book_name"]
+        author = info["data"]["book_info"]["author"]
+        status = int(info["data"]["book_info"]["creation_status"])
+        items = [chapter["item_id"] for chapter in info["data"]["item_data_list"]]
+        if len(items) > 3000:
+            logger.warning(f"Task {task_id} failed: Too many chapters, {len(items)} chapters")
+            raise TooManyChaptersError(f"小说{book_id}章节数量过多，共{len(items)}章，超过3000章限制")
+
+        # 获取已有章节
+        rows = db.query(Chapters).filter(Chapters.book_id == book_id).all()
+        if rows:
+            logger.debug(f"Task {task_id}: Found {len(rows)} existed chapters")
+            existed_chapters_list = [row.item_id for row in rows if row.item_id in items]
+            chapters: dict[str, str] = {
+                row.item_id: row.content
+                for row in rows
+                if row.item_id in items
+            }
+            # 比较已有章节中是否有缺失
+            diff = list(set(items) - set(existed_chapters_list))
+        else:
+            # 无已有章节
+            logger.debug(f"Task {task_id}: No existed chapters")
+            diff = items
+            chapters: dict[str, str] = {}
+
+        # 获取缺失章节内容
+        raw_chapters: list[RawChapter] = await utils.get_chapters(diff)
+        # 格式化章节内容
+        new_chapters: dict[str, str] = {chapter["item_id"]: await utils.format_chapter_content(chapter)
+                                        for chapter in raw_chapters}
+        # 将新章节加入数据库  # TODO：根据是否完结决定是否加入数据库
+        for item_id, content in new_chapters.items():
+            db.add(Chapters(book_id=book_id, item_id=item_id, content=content))
+        db.commit()
+        logger.debug(f"Added {len(new_chapters)} new chapters to database")
+
+        # 合并已有章节和新章节
+        chapters.update(new_chapters)
+        # 合并为完整小说文本
+        content = await utils.format_content(items, title, author, chapters)
+
+        # 保存到缓存
+        await redis_conn.setex(f"book:{book_id}", timedelta(hours=config.ttls.contents), content)
+        # 如果完结保存到数据库
+        if status == 0:
+            logger.debug(f"Task {task_id}: Saving book to database")
+            db.add(Books(book_id=book_id, content=content))
+            db.commit()
+    else:
+        logger.debug(f"Task {task_id}: Found book {book_id} in cache or database")
+        content = result
+    content = content.encode("utf-8") if not isinstance(content, bytes) else content
+    # 过去12小时内下载过的相同小说，或曾经下载过的已完结的小说，尝试获取文件ID，如果不存在则上传
+    if result:
+        try:
+            oclient.get_file_id(f"/{config.onedrive.root}/{book_id}.txt")
+        except Exception:  # noqa: 无法获取文件ID，说明文件不存在
+            oclient.upload_big_file(content, f"/{config.onedrive.root}/{book_id}.txt")
+    url = oclient.get_temp_link(f"/{config.onedrive.root}/{book_id}.txt")
+
+    # for i in range(1, 11):
+    #     # 异步更新进度
+    #     await redis_conn.hset(f"task:{task_id}", "progress", f"{i*10}%")
+
+    # 异步标记任务完成
+    await redis_conn.hset(f"task:{task_id}", mapping={
+        "status": "completed",
+        "content": content,
+        "url": url
+    })
+    await redis_conn.expire(f"task:{task_id}", timedelta(hours=config.ttls.tasks))
+
+
+if __name__ == "__main__":
+    # 启动异步事件循环
+    asyncio.run(main())
