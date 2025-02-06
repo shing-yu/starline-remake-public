c1b9ffc3921e11ba6ba5a7bb0a1165cd11265489
shingyu <shyu@staredges.cn>
2025-02-06 21:22:43 +0800

    update

---
+++ start.zsh
@@ -0,0 +1,3 @@
+sudo systemctl start starline-api
+sudo systemctl start starline-worker
+sudo systemctl start starline-bot
---
+++ stop.zsh
@@ -0,0 +1,3 @@
+sudo systemctl stop starline-api
+sudo systemctl stop starline-worker
+sudo systemctl stop starline-bot
