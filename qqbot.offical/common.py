2da5ffe3491291ca32b6653317efe9819f836fa6
shingyu <shyu@staredges.cn>
2025-02-12 18:20:25 +0800

    Revert "!update"
    This reverts commit 4a108558bc180c3c7e9f65e2ff682110bca21332.

--- qqbot.offical/common.py
+++ qqbot.offical/common.py
@@ -26,8 +26,6 @@ def bookid_parser(book_id: str) -> str:
             book_id = re.search(r"book_id=(\d+)&", book_id).group(1)
         if book_id.isdigit() and len(book_id) == 19:
             return book_id
-        elif book_id == "727241257177946":  # TODO: 提审限制
-            return "7272412571779468340"
         else:
             return ""
     except AttributeError:
